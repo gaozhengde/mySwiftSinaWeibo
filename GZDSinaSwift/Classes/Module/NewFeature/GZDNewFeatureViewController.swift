9755f04e011594f21c2624213bf34fe0003c3aa3
bajie <bajie@qq.com>
2015-10-31 01:55:31 +0800

    新特性界面 完成

--- GZDSinaSwift/Classes/Module/NewFeature/GZDNewFeatureViewController.swift
+++ GZDSinaSwift/Classes/Module/NewFeature/GZDNewFeatureViewController.swift
@@ -28,8 +28,8 @@ class GZDNewFeatureViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        self.collectionView!.registerClass(GZDCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         prepareLayout()
         
@@ -62,10 +62,115 @@ class GZDNewFeatureViewController: UICollectionViewController {
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! GZDCollectionViewCell
     
-        cell.backgroundColor = UIColor.randomColor()
-    
+//        cell.backgroundColor = UIColor.randomColor()
+        
+            cell.imageIndex = indexPath.item
+        
+//            cell.startButtonAnimation()
+        
         return cell
     }
+    //在cell 停止显示的  就是已经显示完成的时候调用
+    override func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+            //拿到当前正在显示的item 的indexPath
+        let showIndexPath :NSIndexPath = collectionView.indexPathsForVisibleItems().first!
+        let cell = collectionView.cellForItemAtIndexPath(showIndexPath) as! GZDCollectionViewCell
+        
+        if showIndexPath.item == itemCount - 1{
+
+            cell.startButton.hidden = false
+            cell.startButtonAnimation()
+        }
+        
+    }
+    
+    
 }
+
+class GZDCollectionViewCell :UICollectionViewCell {
+    
+    //要知道是第几张 然后设置 图片
+    
+    var imageIndex :Int = 0 {
+        
+        didSet{
+            backView.image = UIImage(named: "new_feature_\(imageIndex + 1)")
+            
+            startButton.hidden = true
+        }
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        prepareUI()
+    }
+
+    private func prepareUI()    {
+        
+        contentView.addSubview(backView)
+        contentView.addSubview(startButton)
+        
+        //添加约束
+        
+        backView.translatesAutoresizingMaskIntoConstraints = false
+        
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+//    backView.frame = contentView.bounds
+        contentView.addConstraints( NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[bkg]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["bkg" :backView]))
+        contentView.addConstraints( NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[bkg]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["bkg" :backView]))
+        
+            contentView.addConstraint(NSLayoutConstraint(item: startButton, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: contentView, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: -160))
+        
+            contentView.addConstraint(NSLayoutConstraint(item: startButton, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: contentView, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0))
+
+        
+    }
+    
+    //动画效果的 方法
+    
+    func startButtonAnimation() {
+        startButton.transform = CGAffineTransformMakeScale(0, 0)
+        
+        UIView.animateWithDuration(1, delay: 0.1, usingSpringWithDamping: 0.6, initialSpringVelocity: 6, options: UIViewAnimationOptions(rawValue: 0), animations: { () -> Void in
+            
+           self.startButton.transform = CGAffineTransformIdentity
+            
+            }) { (_) -> Void in
+                
+        }
+    }
+    
+    
+    
+    
+    lazy var backView : UIImageView = UIImageView()
+    
+    private lazy var startButton :UIButton = {
+       
+        let button = UIButton()
+        
+       button.setBackgroundImage(UIImage(named: "new_feature_finish_button"), forState: UIControlState.Normal)
+         button.setBackgroundImage(UIImage(named: "new_feature_finish_button_highlighted"), forState: UIControlState.Highlighted)
+        
+        button.setTitle("开始微博", forState: UIControlState.Normal)
+
+        return button
+    }()
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+
+
+
+
